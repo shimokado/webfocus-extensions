6fb231003e137c7529a2e60c2a256dc2879f64c6
Shimokado Masataka <shimokado@accs.co.jp>
2025-12-03 16:06:31 +0900

    不要ファイルの削除

--- webfocus-webjars-tdg-9.3.2/META-INF/resources/tdg/jschart/distribution/extensions/com.esri.map/lib/layerInfo.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 1996-2023. Cloud Software Group, Inc. All rights reserved. */
-/* $Revision: 1.1 $: */
-(function() {
-	tdgchart.prototype.gisGetExternalLayers = getExternalLayers;
-
-	function getExternalLayers() {
-		return [];
-	}
-}());
